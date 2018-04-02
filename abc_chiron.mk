01e0d45e204c1ea259d484aad9d974daf231d1e0
andr68rus <ashtmb@gmail.com>
2018-04-02 13:36:26 +0300

    chiron: update fingerprint to 8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA
    Change-Id: Ic93984b1f5bb9fb447838dee74c03ddd2d443d5b

--- abc_chiron.mk
+++ abc_chiron.mk
@@ -9,8 +9,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/chiron/chiron:7.1.1/NMF26X/V9.2.3.0.NDEMIEK:user/release-keys \
-    PRIVATE_BUILD_DESC="chiron-user 7.1.1 NMF26X V9.2.3.0.NDEMIEK release-keys"
+    BUILD_FINGERPRINT="Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys" \
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
