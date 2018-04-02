77d4e46aece046d8dd0e3f1a87c29230588ea43d
Wight554 <wight554@gmail.com>
2018-04-02 13:17:37 +0000

    chiron: Initial ABC bringup
    * Cleanup unsupported overlays
    * Remove unused dependencies
    * Add proper HWUI and Dalvik props
    * Add screen width for bootanimation

--- abc_chiron.mk
+++ abc_chiron.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
@@ -13,5 +10,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
+    BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
 
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
+
