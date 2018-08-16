13f81b973cb2ae9314e625438e68469addf2876e
Vol <wight554@gmail.com>
2018-08-16 15:11:04 +0000

    chiron: PePie

--- aosp_chiron.mk
+++ aosp_chiron.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := aosp_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
@@ -15,3 +15,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+
+TARGET_BOOT_ANIMATION_RES := 1080
